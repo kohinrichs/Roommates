7cd2f9caa494084a43f3e67c8c89d2c1236d5854
Katherine Hinrichs <ko.hinrichs@gmail.com>
2021-03-18 13:34:37 -0600

    Finished chore

--- Roommates/Repositories/ChoreRepository.cs
+++ Roommates/Repositories/ChoreRepository.cs
@@ -48,5 +48,51 @@ namespace Roommates.Repositories
                 }
             }
         }
+
+    public Chore GetById(int id)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = "SELECT Name FROM Chore WHERE Id = @id";
+                    cmd.Parameters.AddWithValue("@id", id);
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    Chore chore = null;
+
+                    if (reader.Read())
+                    {
+                        chore = new Chore
+                        {
+                            Id = id,
+                            Name = reader.GetString(reader.GetOrdinal("Name"))
+                        };
+                    }
+                    reader.Close();
+
+                    return chore;
+                }
+            }
+        }
+
+        public void Insert(Chore chore)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"INSERT INTO Chore (Name)
+                                            OUTPUT INSERTED.Id
+                                            VALUES (@name)";
+                    cmd.Parameters.AddWithValue("@name", chore.Name);
+                    int id = (int)cmd.ExecuteScalar();
+
+                    chore.Id = id;
+                }
+            }
+        }
     }
 }
